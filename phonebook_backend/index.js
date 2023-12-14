c4836e9c91230e433bae65b45d806a80db957b26
dimitrispax <dimpax04@gmail.com>
2023-12-14 16:59:22 +0200

    finished exercise 3.5

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -25,12 +26,45 @@ const persons = [
     }
 ]
 
+/* CREATE */
+app.post('/api/persons',(request, response) => {
+
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 200),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+/* READ */
 app.get('/', (request, response)=>{
     response.send('<h1>Hello, you\'ve reached the Root of the server.')
 })
 
 app.get('/api/persons',(request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id',(request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person)
+    response.json(person)
+  else 
+    response.status(404).end()
 })
 
 app.get('/api/info',(request, response) => {
@@ -40,6 +74,14 @@ app.get('/api/info',(request, response) => {
   )
 })
 
+/* DELETE */
+app.delete('/api/persons/:id',(request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+
+})
 
 const PORT = 3001
 app.listen(PORT, ()=>{
