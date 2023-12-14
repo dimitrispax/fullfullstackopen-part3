3715bc05f24548ac4ce885608f1a27e10a27c9d0
dimitrispax <dimpax04@gmail.com>
2023-12-14 20:23:50 +0200

    set files to upload on Render

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
 
 morgan.token('req',  (req, res) => {
   return JSON.stringify(req.body)
@@ -96,7 +97,7 @@ app.delete('/api/persons/:id',(request, response) => {
 
 })
 
-const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}.`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`)
 })
