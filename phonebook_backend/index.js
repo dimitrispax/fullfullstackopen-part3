5c59e331aabcb0f6092f790a281715101894d76f
dimitrispax <dimpax04@gmail.com>
2023-12-14 18:53:57 +0200

    finished exercise 3.8

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -3,7 +3,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+
+morgan.token('req',  (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req'))
 
 let persons = [
     { 
