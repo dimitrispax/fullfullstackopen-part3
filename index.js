ddc9c13b240af57af95d70a3dbe6e655ed582c2d
dimitrispax <dimpax04@gmail.com>
2023-12-15 20:39:12 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 morgan.token('req',  (req, res) => {
   return JSON.stringify(req.body)
